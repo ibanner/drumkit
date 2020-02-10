285123dd2e3f544dabde9efa34fbe354e30c7155
Itay Banner <itay@ibanner.co.il>
2020-02-10 21:22:25 +0200

    bind keydown events to drums

--- index.js
+++ index.js
@@ -1,41 +1,48 @@
 var drums = document.querySelectorAll("button.drum");
 
+function playDrum(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+    case "k":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+    case "l":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+    default:
+      console.log(key);
+  }
+}
+
 for (var i = 0; i < drums.length; i++) {
   drums[i].addEventListener("click", function() {
     this.style.color = "#fff";
     var btnKey = this.innerText;
-    switch (btnKey) {
-      case "w":
-        var tom1 = new Audio('sounds/tom-1.mp3');
-        tom1.play();
-        break;
-      case "a":
-        var tom2 = new Audio('sounds/tom-2.mp3');
-        tom2.play();
-        break;
-      case "s":
-        var tom3 = new Audio('sounds/tom-3.mp3');
-        tom3.play();
-        break;
-      case "d":
-        var tom4 = new Audio('sounds/tom-4.mp3');
-        tom4.play();
-        break;
-      case "j":
-        var snare = new Audio('sounds/snare.mp3');
-        snare.play();
-        break;
-      case "k":
-        var kick = new Audio('sounds/kick-bass.mp3');
-        kick.play();
-        break;
-      case "l":
-        var crash = new Audio('sounds/crash.mp3');
-        crash.play();
-        break;
-      default:
-        var tom1 = new Audio('sounds/tom-1.mp3');
-        tom1.play();
-    }
+    playDrum(btnKey);
   });
 }
+
+document.addEventListener("keydown",function(e) {
+  playDrum(e.key);
+});
